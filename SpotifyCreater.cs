ac476b47e618e0ca79c40b6e91320b4de1673574
michal-nawrocki <michal.m.nawrocki@gmail.com>
2019-04-24 02:32:14 +0200

    Can now Create, FindSong and AddSong

--- SpotifyCreater.cs
+++ SpotifyCreater.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,13 +10,13 @@ using SpotifyAPI.Web.Enums;
 using SpotifyAPI.Web.Models;
 
 namespace yt2spotify{
-
     class SpotifyCreater{
+
+        private static string profileId = "";
+        private static string playlistId = "";
 
         // ReSharper disable once UnusedParameter.Local
         static void Main(string[] args){
-
-
             Console.WriteLine("####### Spotify API Example #######");
             Console.WriteLine("This example uses AuthorizationCodeAuth.");
 
@@ -30,8 +31,7 @@ namespace yt2spotify{
             auth.Stop(0);
         }
 
-        private static async void AuthOnAuthReceived(object sender, AuthorizationCode payload)
-        {
+        private static async void AuthOnAuthReceived(object sender, AuthorizationCode payload){
             AuthorizationCodeAuth auth = (AuthorizationCodeAuth) sender;
             auth.Stop();
 
@@ -42,21 +42,47 @@ namespace yt2spotify{
                 TokenType = token.TokenType,
 
             };
-            CreatePlaylist(api);
+            string playlistid = await CreatePlaylist(api);
+            string songid = await GetSongID(api, "Hallelujah");
+            await AddSongToPlaylist(api, playlistId, songid);
         }
 
-        private static async void CreatePlaylist(SpotifyWebAPI api)
-        {
+        private static async Task<string> CreatePlaylist(SpotifyWebAPI api){
             PrivateProfile profile = await api.GetPrivateProfileAsync();
             string name = string.IsNullOrEmpty(profile.DisplayName) ? profile.Id : profile.DisplayName;
-            string profileId = profile.Id;
+            profileId = profile.Id;
             Console.WriteLine($"Hello there, {name}!");
 
             Console.WriteLine("Will create a new playlist with name \"Test xD\"");
             
             FullPlaylist playlist = api.CreatePlaylist(profileId, "Test xD", false);
             if(!playlist.HasError())
+            //Store the ID for future use
+            playlistId = playlist.Id; 
             Console.WriteLine("Playlist-URI: " + playlist.Uri);
+            Console.WriteLine("Playlist-ID: " + playlist.Id);
+
+            return playlist.Id;
+        }
+
+        private static async Task<String> GetSongID(SpotifyWebAPI api, string songName){
+            SearchItem item = await api.SearchItemsAsync(songName, SearchType.Track, 10);
+
+            foreach(var result in item.Tracks.Items){
+                Console.WriteLine("Name:" + result.Name 
+                    + ", Author:" + result.Artists[0].Name + ", ID:" + result.Id);
+            }
+
+            return item.Tracks.Items[2].Uri;
+        }
+
+        private static async Task AddSongToPlaylist(SpotifyWebAPI api, string playlistID, string songID){
+            ErrorResponse response = await api.AddPlaylistTrackAsync(playlistID, songID);
+            if(!response.HasError()){
+                Console.WriteLine("Success");
+            }else{
+                Console.WriteLine(response.Error.Message);
+            }
         }
     }
 }
